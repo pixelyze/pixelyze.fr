a0604aceec33ccac619744b574ba415fc1391785
pixelyze <pixelyze@gmail.com>
2014-11-17 02:33:20 +0100

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,9 +4,34 @@
 	 * Preloader  
 	/* ---------------------------------------------- */ 
 
+
 	$(window).load(function() { 
 		$('.preloader').fadeOut(1000); // set duration in brackets
-	}); 
+		console.log("jQuery est prêt !");
+
+
+
+	});
+
+
+
+
+	/*$(window).ready(function(){
+	     $('.flexslider').flexslider({
+	         animation: "slide",
+	         slideshow: false,
+	         initDelay: '0',
+	         useCSS: true,
+	         touch: true,
+	     });
+	 }); */
+
+	$(document).ready(function(){
+
+	 });
+
+	
+
 
 	$(document).ready(function() {
 
@@ -36,9 +61,36 @@
 		wow.init();
 
 
+		function initFlexModal2() {
+		    $('#flex2').flexslider({
+			animation: "fade",
+			slideshowSpeed: '3000',
+			slideshow: true,
+			initDelay: '2',
+			useCSS: true,
+			touch: true,
+			pausePlay: true,
+			
+		    });
+		};
+
+
+		$('#portfolioModal7').on('show.bs.modal', function () {
+		   initFlexModal2();
+		   console.log("flex est ok");
+
+		})
+	
+
 		/* ---------------------------------------------- /*
 		 * Slider
-		/* ---------------------------------------------- */
+		/* ---------------------------------------------- 
+		// Can also be used with $(document).ready()
+		  $('.flexslider').flexslider({
+		    animation: 'slide',
+		    slideshow: false,
+		    slideshowSpeed: 7000,
+		  });*/
 
 	});
 
